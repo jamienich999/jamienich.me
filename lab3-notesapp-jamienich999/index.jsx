5b37dbd6248eab4a9f7768f74f1ed9a0b8fea43d
Jamie Nicholson <43357376+jamienich999@users.noreply.github.com>
2024-04-22 22:33:53 -0400

    Delete lab3-notesapp-jamienich999 directory

--- lab3-notesapp-jamienich999/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles.scss';
-import MyApp from './src/app';
-
-const root = createRoot(document.getElementById('main'));
-root.render(<MyApp />);
--- lab3-notesapp-jamienich999/src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function MyButton() {
-    return (
-        <button>testing!</button>
-    );
-}
-
-export default MyButton;
